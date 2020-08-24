e9dc712aaa9cc72aa5da18b4f8a4d2b5e052eeac
Brett Sutton <bsutton@noojee.com.au>
2020-08-24 12:55:35 +1000

    Added support for 'exectuables' map in the pubspec.

--- test/pubspec_test.dart
+++ test/pubspec_test.dart
@@ -7,12 +7,10 @@ import 'package:test/test.dart';
 
 @Skip('not a real test')
 main() async {
-  final PubSpec pubSpec = new PubSpec(
-      name: 'fred',
-      dependencies: {
-        'foo': new PathReference('../foo'),
-        'fred': new HostedReference(new VersionRange(min: new Version(1, 2, 3)))
-      });
+  final PubSpec pubSpec = new PubSpec(name: 'fred', dependencies: {
+    'foo': new PathReference('../foo'),
+    'fred': new HostedReference(new VersionRange(min: new Version(1, 2, 3)))
+  });
 
   await pubSpec.save(await Directory.systemTemp.createTemp('delme'));
 }
